919827dfaf8e4363556a398fef3269a8c4e50126
BHARADWAJ AYINAPURAPU <51354461+BharadwajAyinapurapu@users.noreply.github.com>
2022-09-22 11:24:30 +0530

    Update sonarQube.groovy

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -1,4 +1,3 @@
 def sonarAnalysis(String key, String name, String url, String login){
       sh "mvn sonar:sonar -Dsonar.projectKey=${key}	-Dsonar.projectName=${name} -Dsonar.host.url=${url}  -Dsonar.login=${login}"
 }
-//spring,spring,http://20.244.33.251:9000,9fe0b448d589e6dde9f405e50ed8448f012f09f5
