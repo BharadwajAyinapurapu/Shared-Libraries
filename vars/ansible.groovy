1faf91e1586a00cddb88099eb45e60b5ad05db00
BHARADWAJ AYINAPURAPU <51354461+BharadwajAyinapurapu@users.noreply.github.com>
2022-09-26 10:30:47 +0530

    Update ansible.groovy

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -2,6 +2,6 @@ def ping(String pathOfKey, String hosts){
   sh "ansible ${hosts} -m ping --key-file ${pathOfKey}"
 }
 
-def playbook-execution(String pathOfKey, String playbook){
+def playbook_execution(String pathOfKey, String playbook){
   sh "ansible-playbook --key-file ${pathOfKey} ${playbook}"
 }
